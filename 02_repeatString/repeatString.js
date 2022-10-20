cb13b0c7a8cbcf3225f13127e9b92a01f00acb36
rwxdan <dan1elcastro@proton.me>
2022-10-20 06:06:05 -0400

    Solve repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+	let count = 0;
+	let word = string;
+	while (count < num) {
+		count++;
+	}
+	if (num < 0) {
+		return "ERROR";
+	} else {
+		return word.repeat(count);
+	}
 };
 
 // Do not edit below this line
