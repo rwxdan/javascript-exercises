328887519f59953aff11844a0692faf4b6dd7516
rwxdan <dan1elcastro@proton.me>
2022-10-20 13:24:10 -0400

    Solve removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (numArray, ...args) {
+	args.forEach((item) => {
+		if (numArray.includes(item)) {
+			numArray.splice(numArray.indexOf(item), 1);
+		}
+	});
+	return numArray;
 };
 
 // Do not edit below this line
