54ff2771e6c73966bb4d3e432f076747bd600671
rwxdan <dan1elcastro@proton.me>
2022-10-20 06:44:02 -0400

    Solve reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+	stringArray = string.split("");
+	yarrA = stringArray.reverse();
+	gnirts = yarrA.join("");
+	return gnirts;
 };
 
 // Do not edit below this line
