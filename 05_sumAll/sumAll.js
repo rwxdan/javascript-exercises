db102f658c9eafef426b39984a6a4caf88e11cd2
rwxdan <dan1elcastro@proton.me>
2022-10-20 15:59:23 -0400

    Solve sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function (fromNum, toNum) {
+	let numArr = [];
+	let sumArr = 0;
 
+	if (typeof fromNum != "number" || typeof toNum != "number") {
+		return "ERROR";
+	} else if (fromNum < 0 || toNum < 0) {
+		return "ERROR";
+	} else if (fromNum < toNum) {
+		for (let items = fromNum; items <= toNum; items++) {
+			numArr.push(items);
+		}
+	} else if (fromNum > toNum) {
+		for (let items = fromNum; items >= toNum; items--) {
+			numArr.push(items);
+		}
+	}
+
+	for (let i = 0; i < numArr.length; i++) {
+		sumArr += numArr[i];
+	}
+	return sumArr;
 };
 
 // Do not edit below this line
