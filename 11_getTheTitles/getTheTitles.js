96645ae0b08b21ac41b1b38c210ff9ff597e1e93
Daniel Castro <dan1elcastro@proton.me>
2022-12-05 17:16:38 -0400

    Solve 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (obj) {
+  let arr = [];
+  for (let i = 0; i < obj.length; i++) {
+    arr.push(obj[i].title);
+  }
+  return arr;
 };
-
 // Do not edit below this line
 module.exports = getTheTitles;
