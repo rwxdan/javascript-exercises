7a1d5113ba5b44e6b63346b9f324c2e924744067
Daniel Castro <dan1elcastro@proton.me>
2022-12-05 16:07:51 -0400

    Solve 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
-
+const palindromes = function (arg) {
+  let a = "!";
+  let b = ",";
+  let c = ".";
+  let d = " ";
+  let argRev = arg.toLowerCase().split("").reverse();
+  argRev = argRev.filter((i) => {
+    if (i != a && i != b && i != c && i != d) {
+      return i;
+    }
+  });
+  let argClean = arg.toLowerCase().split("");
+  argClean = argClean.filter((i) => {
+    if (i != a && i != b && i != c && i != d) {
+      return i;
+    }
+  });
+  return argRev.join("") == argClean.join("");
 };
 
 // Do not edit below this line
