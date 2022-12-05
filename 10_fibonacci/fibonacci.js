3cd42ce073f349a98414565a4b8897cfcf158c16
Daniel Castro <dan1elcastro@proton.me>
2022-12-05 17:02:07 -0400

    Solve 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function (value) {
+  value = Number(value);
+  if (value < 0) {
+    return "OOPS";
+  } else if (value == 1) {
+    return 1;
+  }
+  let a = 0,
+    b = 1,
+    c,
+    d;
+  for (let i = 1; i <= value; i++) {
+    d = c;
+    c = a + b;
+    a = b;
+    b = c;
+  }
+  return d;
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
